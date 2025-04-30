fda3d176651520fd050bedde47427814bfc5d3ae
lkirk <kirklloyd@gmail.com>
2025-04-30 15:32:59 -0500

    get rid of corfu and cape for now, no terminal completion

--- lisp/lk-dev.el
+++ lisp/lk-dev.el
@@ -18,22 +18,33 @@
 (use-package prescient)
 (use-package vertico-prescient)
 
-(use-package
- corfu
- :config
- :custom (corfu-auto t) (corfu-quit-no-match 'separator)
- :init (declare-function global-corfu-mode "corfu") (global-corfu-mode))
+;; (declare-function corfu-prescient-mode "corfu")
+;; (declare-function global-corfu-mode "corfu")
+;; (declare-function cape-wrap-buster "cape")
 
-;; Add extensions
-(use-package
- cape
- :bind ("C-c p" . cape-prefix-map)
- :init
- (add-hook 'completion-at-point-functions #'cape-tex)
- (add-hook 'completion-at-point-functions #'cape-file)
- (add-hook 'completion-at-point-functions #'cape-keyword)
- (add-hook 'completion-at-point-functions #'cape-history))
-;; (add-hook 'completion-at-point-functions #'cape-history)
+;; (use-package
+;;  corfu
+;;  ;; :after (eglot cape prescient)
+;;  :after (eglot prescient)
+;;  :custom (corfu-auto t)
+;;  ;; (corfu-quit-no-match 'separator "Quit at separator if no match")
+;;  (corfu-quit-no-match nil)
+;;  (corfu-cycle t "Enable cycling for `corfu-next/previous'") ;; 
+;;  (corfu-preselect nil "Do not preselect prompt")
+;;  ;; :bind (:map corfu-mode-map ("TAB" . #'corfu-complete))
+;;  (corfu-prescient-mode 1)
+;;  :config (global-corfu-mode))
+
+(use-package company :hook (after-init . global-company-mode))
+
+;; (use-package
+;;  cape
+;;  :bind ("C-c p" . cape-prefix-map)
+;;  :init
+;;  (add-hook 'completion-at-point-functions #'cape-tex)
+;;  (add-hook 'completion-at-point-functions #'cape-file)
+;;  (add-hook 'completion-at-point-functions #'cape-keyword)
+;;  (add-hook 'completion-at-point-functions #'cape-history))
 
 (use-package
  flymake
@@ -63,19 +74,18 @@
   'flymake-margin-string
   (alist-get 'note flymake-margin-indicators-string)))
 
-(defun lk/format-on-save ()
-  "Format buffer if not in `cc-mode`."
-  ;; Disable so that I can use clang-format in tskit development
-  (unless (or (string-match "repo/tskit" buffer-file-name)
-              (derived-mode-p 'wolfram-mode))
-    (when (eglot-managed-p) ;; prevent jsonrpc errors
-      (eglot-format))))
-
 (use-package
  eglot
  :init
  (declare-function eglot-managed-p "eglot")
  (declare-function eglot-format "eglot")
+ (defun lk/format-on-save ()
+   "Format buffer if not in `cc-mode`."
+   ;; Disable so that I can use clang-format in tskit development
+   (unless (or (string-match "repo/tskit" buffer-file-name)
+               (derived-mode-p 'wolfram-mode))
+     (when (eglot-managed-p) ;; prevent jsonrpc errors
+       (eglot-format))))
  :ensure t
  :hook (before-save . lk/format-on-save)
  ;; (prog-mode . lk/eglot-ensure)
@@ -85,10 +95,15 @@
  (eglot-report-progress nil)
  (eglot-extend-to-xref 1)
  :config
+ ;; Enable cache busting, depending on if your server returns
+ ;; sufficiently many candidates in the first place.
+ (with-eval-after-load 'eglot
+   (setq completion-category-defaults nil))
+ (add-to-list 'eglot-server-programs '(vala-mode . ("vala-language-server")))
+ (add-to-list 'eglot-server-programs '(meson-mode . ("mesonlsp")))
  (add-to-list 'eglot-server-programs '(fish-mode . ("fish-lsp" "start")))
  (add-to-list 'eglot-server-programs '(awk-mode . ("awk-language-server")))
  (add-to-list 'eglot-server-programs '(LaTeX-mode . ("texlab")))
-
  (let ((mode '((wolfram-mode :language-id "Wolfram Language")))
        ;; (wolfram-lsp-cmd '("wolframscript" "-code"
        ;;                    "Needs[\"LSPServer`\"];LSPServer`StartServer[]")))
