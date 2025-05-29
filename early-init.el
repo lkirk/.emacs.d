cc57fe7a89f97c064fccec1eac129a03905e12a8
lkirk <kirklloyd@gmail.com>
2025-05-29 12:30:25 -0500

    add comment structure

--- early-init.el
+++ early-init.el
@@ -1,6 +1,16 @@
-;; early-init.el -*- lexical-binding: t; -*-
+;;; early-init.el --- -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Turn off package, in favor of elpaca.  Increase gc-cons-threshold
+;; to improve performance (unmeasured).
+
+;;; Code:
 
 (setq package-enable-at-startup nil)
 
 ;; Performance?
 (setq gc-cons-threshold 100000000)
+
+(provide 'init)
+;;; early-init.el ends here
